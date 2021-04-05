740b8bcdaba061dc12ce0830f30d87ca675eac01
Soria <soriasayer@gmail.com>
2021-04-05 21:30:39 +0200

    Using costom hooks.

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,53 +1,17 @@
-import React, { useEffect } from 'react';
-import { useQuery, useMutation, useSubscription } from '@apollo/react-hooks';
-import { addMessage, addMessageMutation, getMessages, messageAddedSubscription, messagesQuery, onMessageAdded } from './graphql/queries';
+import React from 'react';
 import MessageInput from './MessageInput';
 import MessageList from './MessageList';
+import {useChatMessages} from './hooks';
 
 const Chat = ({user}) => {
-  const {data} = useQuery(messagesQuery)
-  const messages = data ? data.messages : []
-  useSubscription(messageAddedSubscription, {
-    onSubscriptionData: ({client, subscriptionData}) => {
-      client.writeData({data: {
-        messages: messages.concat(subscriptionData.data.messageAdded)
-      }})
-    }
-  })
-  const [addMessage] = useMutation(addMessageMutation)
- 
-  
-  // useEffect(() => {
-  //   let subscription = null
-    
-  //   async function fetchMessage() {
-  //     const msgs = await getMessages();
-  //     setMessage(msgs);
-      
-  //     subscription = await onMessageAdded(message => {
-  //       setMessage(m => [...m, message]);
-  //     })
-      
-  //   }
-  //   fetchMessage()
-
-  //   return () => {
-  //     if(subscription) {
-  //       subscription.unsubscribe()
-  //     }
-  //   }
-  // },[])
-
-  const handleSend = async(text) => {
-    await addMessage({variables: {input: {text}}})
-  }
+  const {messages, addMessage} = useChatMessages()
 
   return (
     <section className="section">
       <div className="container">
         <h1 className="title">Chatting as {user}</h1>
         <MessageList user={user} messages={messages} />
-        <MessageInput onSend={handleSend} />
+        <MessageInput onSend={addMessage} />
       </div>
     </section>
   );  
